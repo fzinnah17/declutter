44fe544c0cf0b48f6fefed4ca2b8d0e774603ea1
fzinnah17 <fzinnah000@citymail.cuny.edu>
2021-08-15 02:12:59 -0400

    changes with style and checkbox. Working on more changes

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,12 +1,22 @@
+import './ToDo.css'
 import React from 'react';
 
-export default function Todo({ title, completed }) {
+export default function Todo({ title, completed}) {
     return (
+        <form>
+            <div class="inline-field">
+            <input type="checkbox" id="checkbox" name="accept" value="yes">
+            </input>
         <label
-            className= "list-group-item d-flex justify-content-between my-2">
+            for="checkbox-inline"
+            className= "list-group-item d-flex justify-content-between my-2"
+            >
             <h6 
                 className={`align-middle ${completed ? 'completed-task' : ''}`}
-            >{title}</h6>
+            >{title}
+            </h6>
         </label>
+            </div>
+        </form>
     )
 }
